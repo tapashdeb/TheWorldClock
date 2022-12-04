5e1f4070765133fa292ef612cf5f0463b43486b5
Tapash <tapash.deb1238@gmail.com>
2022-12-04 09:57:36 +0530

    This is first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+setInterval(function(){
+    let d= new Date();
+    let gethrs=d.getHours();
+    let getmints=d.getMinutes();
+    let getsecond=d.getSeconds();
+
+    let hrotation=30*gethrs + getmints/2;
+    let mrotation=6*getmints;
+    let secrotation=6*getsecond;
+    let mysound= new Audio('clock sound.mp3');
+
+    hour.style.transform=`rotate(${30*gethrs + getmints/2}deg)`
+    minutes.style.transform=`rotate(${6*getmints}deg)`
+    second.style.transform=`rotate(${6*getsecond}deg)`
+    mysound.play()
+
+
+},1000)
